a3d71e736fe82709439214df3f86bdc6840c9be6
patrosk <naiade99@gmail.com>
2022-11-18 15:07:49 +0100

    added images and started to work on canvas

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-const Application = PIXI.Application;
-
 const app = new PIXI.Application({
-    width: 500,
-    height: 500
+    width: 350,
+    height: 600,
+    backgroundColor: 0xFFFCF2,
 });
 
 document.body.appendChild(app.view);
+
+const h1Style = new PIXI.TextStyle({
+    fontSize: 36,
+    fontFamily: 'Comfortaa',
+    fill: 0xff8642,
+});
+const h1 = new PIXI.Text('Rim med Kim', h1Style);
+
+
+const kim = PIXI.Sprite.from("./images/kim1.svg");
+kim.anchor.set(0.5);
+kim.height = 210;
+kim.width = 140;
+kim.x = app.view.width / 2;
+kim.y = app.view.height / 1.4;
+
+const startButton = PIXI.Sprite.from("./images/startButton.svg");
+startButton.anchor.set(0.5);
+startButton.height = 50;
+startButton.width = 100;
+startButton.x = app.view.width / 2;
+startButton.y = app.view.height / 5;
+
+app.stage.addChild(startButton);
+app.stage.addChild(h1);
+app.stage.addChild(kim);
